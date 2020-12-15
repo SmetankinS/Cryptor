84e04b7fee9dc295d4e14057c572aeb74ca83751
Виктор Елсуков <beerbis@yandex.ru>
2020-12-15 22:44:26 +0300

    refactor: стильно модно молодёжно

--- src/CryptorFactory.java
+++ src/CryptorFactory.java
@@ -1,5 +1,16 @@
 public class CryptorFactory {
-    public static Cryptor getCryptor (String cryptorId) {
-        return new MyCryptor();
+    public static Cryptor getCryptor(String cryptorId, String key) {
+        switch (cryptorId) {
+            case ScitalCryptor.name:
+            case "default":
+                return new ScitalCryptor(Integer.valueOf(key));
+            default:
+                throw new CryptorFactoryException("Unknown cryptor name: " + cryptorId);
+        }
+
+    }
+
+    public static class CryptorFactoryException extends RuntimeException {
+        public CryptorFactoryException(String message) { super(message); }
     }
 }
